1f6c9a9dd51d4068721a23aeedfe1706e96cc942
Chentro <vasquezsaavedrasergio@gmail.com>
2021-11-30 21:09:45 -0300

    siiiiiuuuuuu

--- app/src/main/java/com/eva/prueba_3/Resulta2.java
+++ app/src/main/java/com/eva/prueba_3/Resulta2.java
@@ -2,18 +2,39 @@ package com.eva.prueba_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Resulta2 extends AppCompatActivity {
 
     int a, b, c, d;
+    TextView txt1, txt2, txt3,txt4;
+    Button Btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resulta2);
+
+        txt1=(TextView) findViewById(R.id.textView2);
+        txt2=(TextView) findViewById(R.id.textView4);
+        txt3=(TextView) findViewById(R.id.textView6);
+        txt4=(TextView) findViewById(R.id.textView8);
+
+        Btn3=(Button) findViewById(R.id.button);
+
+        Btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(I);
+            }
+        });
 
         SQLiteDatabase db;
         conexion conn = new conexion(getApplicationContext());
@@ -48,8 +69,13 @@ public class Resulta2 extends AppCompatActivity {
                 }
                 while (C.moveToNext());
 
-
             }
         }
+
+        txt1.setText(a+"");
+        txt2.setText(b+"");
+        txt3.setText(c+"");
+        txt4.setText(d+"");
+
     }
 }
